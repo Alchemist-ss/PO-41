730f0163bdf8f3581b5d9b9c80771990e393cf19
Alchemist-ss <83163619+Alchemist-ss@users.noreply.github.com>
2021-06-14 17:54:32 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		speed: 500,
+		easding: 'ease',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: false,
+		autoplaySpeed: 1500,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false,
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false,
+		centerMode: true,
+		varibleWidth: false,
+	});
+});
